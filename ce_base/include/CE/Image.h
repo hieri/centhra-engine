4d9c6e005e6816c4793b9702e78e12deae393433
Johnny Patterson <jpatterson@centhra.com>
2013-02-03 00:31:59 -0800

    Documented Image class.

--- ce_base/include/CE/Image.h
+++ ce_base/include/CE/Image.h
@@ -10,8 +10,7 @@
 
 namespace ce
 {
-	/* 	Image Class
-	 * 	- Holds image data.
+	/**	@brief Image Data Structure Class
 	 */
 	class Image
 	{
@@ -22,17 +21,40 @@ namespace ce
 		Image();
 
 	public:
+		/**	@brief Initializes the registered image loaders.
+		 */
 		static bool init();
 
+		/**	@brief File Constructor.
+		 */
 		static Image *createFromFile(const char *file);
+
+		/**	@brief Memory Constructor.
+		 */
 		static Image *createFromMemory(const unsigned char *memory, unsigned int size);
+
+		/**	@brief Pixel Array Constructor.
+		 */
 		static Image *createFromPixels(char *pixels, unsigned char bytesPerPixel, Vector2<int> size);
 
+		/**	@brief Destructor.
+		 */
 		~Image();
 
+		/**	@brief Binds the image for rendering.
+		 */
 		void bind();
+
+		/**	@brief Returns the Color value of a pixel.
+		 */
 		Color getPixel(unsigned int x, unsigned int y) const;
+
+		/**	@brief Returns the size of the image.
+		 */
 		Vector2<int> getSize() const;
+
+		/**	@brief Saves the image to a file.
+		 */
 		bool saveToFile(const char *file) const;
 	};
 }

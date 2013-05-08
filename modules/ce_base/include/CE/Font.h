786c4155dd6fd2aa3dcc7d6b6463cc791bc2e9f2
Johnny Patterson <jpatterson@centhra.com>
2013-05-08 00:00:01 -0700

    ce_base: Documented Font

--- modules/ce_base/include/CE/Font.h
+++ modules/ce_base/include/CE/Font.h
@@ -6,8 +6,7 @@
 
 namespace ce
 {
-	/* 	Font Class
-	 * 	- Acts as specialized handle for different fonts.
+	/**	@brief Image Data Structure Class
 	 */
 	class Font
 	{
@@ -21,18 +20,50 @@ namespace ce
 		Font();
 
 	public:
+		/**	@brief Initializes the registered font loaders.
+		 *	@return Whether or not the font loader initialization succeeded.
+		 */
 		static bool Init();
+
+		/**	@brief File Constructor.
+		 *	@param file Path to font file.
+		 *	@return Image object or 0 if creation failed.
+		 */
 		static Font *CreateFromFile(const char *file);
+
+		/**	@brief Memory Constructor.
+		 *	@param memory Address of font data.
+		 *	@param size Size of font data.
+		 *	@return Image object or 0 if creation failed.
+		 */
 		static Font *CreateFromMemory(const unsigned char *memory, unsigned int size);
 
-		static Font *GetDefault();
-		static void DeleteDefault();
-
+		/**	@brief Destructor.
+		 */
 		~Font();
 
+		/**	@brief Draws a string.
+		 *	@param str The string that will be drawn.
+		 */
 		void DrawString(const char *str);
+
+		/**	@brief Draws a unicode character.
+		 *	@param unicodeChar The unicode character that will be drawn.
+		 */
 		void DrawUnicodeChar(unsigned short unicodeChar);
+
+		/**	@brief Sets the character size of the Font.
+		 *	@param width The width of a character.
+		 *	@param height The height of a character.
+		 *	@param horizontalDpi The horizontal dpi of a character.
+		 *	@param verticalDpi The vertical dpi of a character.
+		 */
 		void SetCharSize(unsigned int width, unsigned int height, unsigned int horizontalDpi, unsigned int verticalDpi);
+
+
+		/**	@brief Returns the character height of the font.
+		 *	@return The character height of the Font.
+		 */
 		unsigned int GetCharHeight() const;
 	};
 }
